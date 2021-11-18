98520e609bd12dbe797c939dda0846420fcd5968
“Yuri <yura.kopshev@gmail.com>
2021-11-18 17:42:36 +0400

    added method security

--- src/main/java/ru/netology/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/ru/netology/SpringSecurity/config/SecurityConfig.java
@@ -2,20 +2,22 @@ package ru.netology.SpringSecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true,jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("yuri").password("{noop}1234").roles("READ")
-                .and().withUser("alex").password("{noop}5678").roles("READ");
+                .and().withUser("alex").password("{noop}5678").roles("WRITE")
+                .and().withUser("serg").password("{noop}9111").roles("DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and().authorizeRequests().antMatchers("/account").permitAll()
-                .and().authorizeRequests().anyRequest().authenticated();
+        http.formLogin().and().authorizeRequests().anyRequest().authenticated();
     }
 }
